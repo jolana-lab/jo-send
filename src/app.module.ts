ef36f397a0f6a87692e8a59abcef3db819af40fa
Jijie LIU <liujijieseason@gmail.com>
2022-02-13 19:23:16 +0100

    feat: init wallet

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,7 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { WalletModule } from './wallet/wallet.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [WalletModule],
 })
 export class AppModule {}
