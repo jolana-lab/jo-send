a926e616d50622fd9f9114c4fea0ca050b2d2d33
Jijie LIU <liujijieseason@gmail.com>
2022-02-13 20:24:10 +0100

    feat: configure env variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,13 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { WalletModule } from './wallet/wallet.module';
 
 @Module({
-  imports: [WalletModule],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: ['.local.env', '.production.env'],
+    }),
+    WalletModule,
+  ],
 })
 export class AppModule {}
