5df38a125d9686971d85ca82d592f096c527e3c4
Jijie LIU <liujijieseason@gmail.com>
2022-02-13 21:22:14 +0100

    feat: encrypt and decrypt using AES

--- src/wallet/wallet.service.ts
+++ src/wallet/wallet.service.ts
@@ -1,10 +1,9 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Keypair } from '@solana/web3.js';
+import { AES, enc } from 'crypto-js';
 import { Model } from 'mongoose';
 import { Wallet, WalletDocument } from './schemas/wallet.schema';
-import * as bcrypt from 'bcrypt';
-
 @Injectable()
 export class WalletService {
   constructor(
@@ -13,13 +12,19 @@ export class WalletService {
 
   async create(username: string): Promise<Wallet> {
     const keypair = Keypair.generate();
-    // hash the secret key
-    const secret: string = await bcrypt.hash(keypair.secretKey.join('-'), 10);
+    const secret = AES.encrypt(keypair.secretKey.join('-'), process.env.NONCE);
 
     const wallet = new this.walletModel({
       username,
       secret,
     });
     return wallet.save();
+  }
+
+  async airdrop(username: string) {
+    const wallet = await this.walletModel.findOne({ username });
+    const secret = AES.decrypt(wallet.secret, process.env.NONCE).toString(
+      enc.Utf8,
+    );
   }
 }
