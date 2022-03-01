5c6733b0b7c353541e3c9c337d8eb0e929b8dd05
Jijie LIU <liujijieseason@gmail.com>
2022-03-01 22:04:08 +0100

    Update slack.service.spec.ts

--- src/slack/slack.service.spec.ts
+++ src/slack/slack.service.spec.ts
@@ -4,6 +4,7 @@ import { WalletService } from '../wallet/wallet.service';
 import { SolanaServiceStub } from '../__mocks__/blockchain/solana.service.stub';
 import { DUMMY_WALLET } from '../__mocks__/wallet/wallet.module.stub';
 import { WalletServiceStub } from '../__mocks__/wallet/wallet.service.stub';
+import { ErrorResponseContent } from './response-contents/error-response-content';
 import { OkResponseContent } from './response-contents/ok-response-content';
 import { SlackService } from './slack.service';
 
@@ -30,18 +31,27 @@ describe('SlackService', () => {
     expect(slackService).toBeDefined();
   });
 
-  it('should send SOL', async () => {
+  describe('should send SOL', () => {
     const fromUsername = 'fromUsername';
     const toUsername = 'toUsername';
     const sol = 1;
-    const expectedResult = new OkResponseContent(
-      `${fromUsername} sent ${sol} SOL to ${toUsername}`,
-    );
 
-    jest.spyOn(walletService, 'get').mockResolvedValue(DUMMY_WALLET);
-    jest.spyOn(solanaService, 'sendSol').mockResolvedValue('OK');
+    it('success', async () => {
+      const expectedResult = new OkResponseContent(
+        `${fromUsername} sent ${sol} SOL to ${toUsername}`,
+      );
 
-    const result = await slackService.sendSol(fromUsername, toUsername, sol);
-    expect(result).toEqual(expectedResult);
+      jest.spyOn(walletService, 'get').mockResolvedValue(DUMMY_WALLET);
+      jest.spyOn(solanaService, 'sendSol').mockResolvedValue('OK');
+
+      const result = await slackService.sendSol(fromUsername, toUsername, sol);
+      expect(result).toEqual(expectedResult);
+    });
+
+    it('should handler internal error', async () => {
+      jest.spyOn(walletService, 'get').mockRejectedValue(new Error('error'));
+      const result = await slackService.sendSol(fromUsername, toUsername, sol);
+      expect(result).toBeInstanceOf(ErrorResponseContent);
+    });
   });
 });
