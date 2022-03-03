ca99ccfe89212eeb21c199f9ae25951a3e09bf9a
Jijie LIU <liujijieseason@gmail.com>
2022-03-03 22:39:48 +0100

    feat: remove wallet controller

--- src/wallet/wallet.module.ts
+++ src/wallet/wallet.module.ts
@@ -2,7 +2,6 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { BlockchainModule } from 'src/blockchain/blockchain.module';
 import { Wallet, WalletSchema } from './schemas/wallet.schema';
-import { WalletController } from './wallet.controller';
 import { WalletService } from './wallet.service';
 
 @Module({
@@ -10,7 +9,6 @@ import { WalletService } from './wallet.service';
     MongooseModule.forFeature([{ name: Wallet.name, schema: WalletSchema }]),
     BlockchainModule,
   ],
-  controllers: [WalletController],
   providers: [WalletService],
   exports: [WalletService],
 })
