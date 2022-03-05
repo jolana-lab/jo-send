1e4514a6c2b1e244a410085135adbaa08ab8e56d
Jijie LIU <liujijieseason@gmail.com>
2022-03-05 11:32:13 +0100

    feat: enable cors protection

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 import helmet from 'helmet';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
   // secure app
   app.use(helmet());
 
