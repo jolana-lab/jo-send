522b986d1539f0d48e7499cc5e606efab26ccd5b
Jijie LIU <liujijieseason@gmail.com>
2022-03-05 11:38:58 +0100

    feat: enable csrf protection

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import helmet from 'helmet';
+import csurf from 'csurf';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
+  await app.listen(3030);
   // secure app
   app.use(helmet());
-
-  await app.listen(3030);
+  app.use(csurf());
 }
 bootstrap();
